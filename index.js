50625029f2f0a88ffa4f51a9060579566371b5b2
Diego Cruz <diego0260203@gmail.com>
2022-09-19 23:04:12 -0300

    create deploy

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+const path = require('path')
+// const PORT = process.env.PORT || 5000
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/reservar.ejs', (req, res) => res.render('pages/reservar'))
+  .get('/servicos.ejs', (req, res) => res.render('pages/servicos'))
+  .get('/hospedagem.ejs', (req, res) => res.render('pages/hospedagem'))
+  .get('/index.ejs', (req, res) => res.render('pages/index'))
+  .listen(process.env.PORT || 5000, () => console.log(`Listening on ${ 1 }`))
